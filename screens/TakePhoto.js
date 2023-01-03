87fd04940872eb8d7bacb0461f69b30dbe7e2c9c
Can Yakar <canyakar9797@gmail.com>
2023-01-03 06:37:26 +0300

    Update TakePhoto.js

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -2,8 +2,6 @@
 import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
-import { shareAsync } from 'expo-sharing';
-import * as MediaLibrary from 'expo-media-library';
 import { CircularButton} from '../components'
 import { assets, COLORS, FONTS, SIZES } from '../constants';
 import axios from 'axios';
@@ -25,21 +23,21 @@ const TakePhoto = () => {
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
   const [photo, setPhoto] = useState();
   const [isLoading, setIsLoading] = useState(false)
+  
+  // useEffect(() => {
+  //   (async () => {
+  //     const cameraPermission = await Camera.requestCameraPermissionsAsync();
+  //     const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+  //     setHasCameraPermission(cameraPermission.status === "granted");
+  //     setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+  //   })();
+  // }, []);
 
-  useEffect(() => {
-    (async () => {
-      const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
-      setHasCameraPermission(cameraPermission.status === "granted");
-      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
-    })();
-  }, []);
-
-  if (hasCameraPermission === undefined) {
-    return <Text>Requesting permissions...</Text>
-  } else if (!hasCameraPermission) {
-    return <Text>Permission for camera not granted. Please change this in settings.</Text>
-  }
+  // if (hasCameraPermission === undefined) {
+  //   return <Text>Requesting permissions...</Text>
+  // } else if (!hasCameraPermission) {
+  //   return <Text>Permission for camera not granted. Please change this in settings.</Text>
+  // }
 
   const takePic = async () => {
     let options = {
@@ -53,7 +51,8 @@ const TakePhoto = () => {
     setIsLoading(true)
   };
 
-  if (photo) {
+
+  const fetchProductData = async () => {
     let data = new FormData();
     data.append('file', { uri: photo.uri, name: 'photo.png', filename: 'imageName.jpg', type: 'image/jpg' });
 
@@ -65,17 +64,26 @@ const TakePhoto = () => {
       }
     };
 
-    axios.post('http://34.240.229.186/photo', data, config)
-    .then( (response) => {
-      setIsLoading(false)
+    try {
+      const response = await axios.post('http://34.240.229.186/photo', data, config);
+      setIsLoading(false);
       for (const product of ProductData) {
         if (product.name === Object.values(response.data)[1]) {
-          navigation2.navigate("ProductDetails", {product, navigation2})
+          navigation2.navigate("ProductDetails", {product, navigation2});
         }
       }
-    })
-    .catch((err)=>setIsLoading(false))
-  }
+    } catch (err) {
+      setIsLoading(false);
+      alert(err)
+    }
+  };
+
+  useEffect(() => {
+    if (photo) {
+      fetchProductData();
+    }
+  }, [photo]);
+
 
   if (isLoading){
     return <LoadingScreen/>
